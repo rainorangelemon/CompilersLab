fd29e62e0d590a75d07f182edd8ec8d95ae9497c
rainorangelemon <rainorangelemon@gmail.com>
2018-05-01 22:02:02 +0800

    finish the output for syntex-correct program

--- Lex/main.c
+++ Lex/main.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../Syntax/syntax.tab.c"
+#include "tree.h"
+
+extern int yylineno;
+extern Node* root;
+extern int errorFlag;
+
+Node* createNode(char* name, char* value){
+    Node* result = (Node*)malloc(sizeof(Node));
+    result -> lineno = yylineno;
+    strcpy(result->name, name);
+    strcpy(result->value, value);
+    result -> son = NULL;
+    result -> bro = NULL;
+    return result;
+}
+
+/*正序插入*/
+void addSon(Node* father, Node* son){
+    if(father!=NULL && son!=NULL){
+        if(father->son!=NULL){
+            Node* p = father->son;
+            while(p->bro!=NULL){
+                p = p->bro;
+            }
+            p->bro = son;
+        }else{
+            father->son = son;
+            father->lineno = son->lineno;
+        }
+    }
+}
+
+/* print the tree*/
+void printTree(Node* root, int n){
+    for(int i=0; i<n; i++)
+        printf(" ");
+    Node* p = root->son;
+    if(p!=NULL){
+    	printf("%s (%d)\n", root->name, root->lineno);
+    }else{
+	if(strcmp(root->name, "FLOAT") == 0){
+	    printf("%s: %f\n", root->name, atof(root->value));
+	}else if(strcmp(root->name, "INT") == 0){
+	    printf("%s: %ld\n", root->name, strtol(root->value, NULL, 0));
+	}else if((strcmp(root->name, "TYPE") == 0)||(strcmp(root->name, "ID") == 0)){
+	    printf("%s: %s\n", root->name, root->value);
+	}else{
+	    printf("%s\n", root->name);
+	}
+    }
+    while(p != NULL){
+        printTree(p, n+1);
+        p = p -> bro;
+    }
+}
+
+/* delete the tree*/
+void deleteTree(Node* root){
+    if(root==NULL){
+        return;
+    }else{
+        deleteTree(root->son);
+        deleteTree(root->bro);
+        free(root);
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -13,5 +78,9 @@ int main(int argc, char* argv[])
   }
   yyrestart(f);
   yyparse();
+  if(errorFlag == 0){
+    printTree(root, 0);
+  }
+  deleteTree(root);
   return 0;
 }
--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:
 	mv syntax.tab.c	syntax.tab.h Syntax
 	flex Lex/lexical.l
 	mv lex.yy.c Lex
-	gcc Lex/main.c Lex/tree.c -ll -ly -o parser
+	gcc Lex/main.c Syntax/syntax.tab.c -ll -ly -o parser
 
 test1:
 	./parser Test/test1.cmm	
